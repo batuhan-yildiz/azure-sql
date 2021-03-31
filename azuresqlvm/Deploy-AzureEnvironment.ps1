6d38eb276ec0f08f53ad138798c098ee6e6b2f2e
byildiz05 <batuhanyildiz@users.noreply.github.com>
2021-03-30 19:37:17 -0700

    commit test 01

--- azuresqlvm/Deploy-AzureEnvironment.ps1
+++ azuresqlvm/Deploy-AzureEnvironment.ps1
@@ -4,3 +4,13 @@
 
 Set-Item -Path Env:\SuppressAzurePowerShellBreakingChangeWarnings -Value $true
 
+$resourceGroup = "azure-data-rg01"
+
+$check = Get-AzContext -ErrorAction SilentlyContinue
+if ($null -eq $check) 
+{
+    Connect-AzAccount -SubscriptionId $subscriptionId | out-null
+}
+else {
+    Set-AzContext -SubscriptionId $subscriptionId | out-null
+}
