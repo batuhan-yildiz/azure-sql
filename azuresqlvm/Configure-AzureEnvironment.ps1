25be2cae03f7d902400b1261fca0a3a93b29db53
byildiz05 <batuhanyildiz@users.noreply.github.com>
2021-04-02 18:03:40 -0700

    configure deployment

--- azuresqlvm/Configure-AzureEnvironment.ps1
+++ azuresqlvm/Configure-AzureEnvironment.ps1
@@ -2,6 +2,8 @@
 # Enable Firewall Rule
 # Install Active Directory Domain Services
 # Create a new Active Directory Organization Unit and make it default for computer objects
+# Join Azure VM to domain
+# Add Firewall Rule
 
 [CmdletBinding()]
 param(
@@ -70,7 +72,7 @@ function EnableFirewallRule
     )
     try {
             # Create a temporary file in the users TEMP directory
-            $file = $env:TEMP + "\FirewallRule.ps1"
+            $file = $env:TEMP + "\EnableFirewallRule.ps1"
 
             $commands = "Enable-NetFirewallRule -DisplayName ""File and Printer Sharing (Echo Request - ICMPv4-In)"""
             $commands | Out-File -FilePath $file -force
@@ -108,7 +110,7 @@ function InstallADDS
     )
     try {
             # Create a temporary file in the users TEMP directory
-            $file = $env:TEMP + "\ADDS.ps1"
+            $file = $env:TEMP + "\InstallADDS.ps1"
 
             $commands = "`$SecurePassword = ConvertTo-SecureString ""$adminPassword"" -AsPlainText -Force" + "`r`n"
             $commands = $commands + "`r`n"
@@ -126,7 +128,7 @@ function InstallADDS
             $commands = $commands + "-ForestMode ""WinThreshold"" ``" + "`r`n"
             $commands = $commands + "-InstallDns:`$true ``" + "`r`n"
             $commands = $commands + "-LogPath ""C:\Windows\NTDS"" ``" + "`r`n"
-            $commands = $commands + "-NoRebootOnCompletion:`$true ``" + "`r`n"
+            $commands = $commands + "-NoRebootOnCompletion:`$false ``" + "`r`n"
             $commands = $commands + "-SafeModeAdministratorPassword `$SecurePassword ``" + "`r`n"
             $commands = $commands + "-SysvolPath ""C:\Windows\SYSVOL"" ``" + "`r`n"
             $commands = $commands + "-Force:`$true"
@@ -163,7 +165,7 @@ function NewADOU
     )
     try {
             # Create a temporary file in the users TEMP directory
-            $file = $env:TEMP + "\ADDS.ps1"
+            $file = $env:TEMP + "\NewADOU.ps1"
 
             $commands = "#Create an OU and make it default computer objects OU" + "`r`n"
             $commands = $commands + "New-ADOrganizationalUnit -Name ""AlwaysOnOU"" -Path ""DC=CONTOSO,DC=COM""" + "`r`n"
@@ -189,23 +191,108 @@ function NewADOU
     }
 }    
 
+# Join Azure VM to domain
+function JoinDomain 
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$vmName,
+        [Parameter(Mandatory = $true)]
+        [string]$resourceGroup,
+        [Parameter(Mandatory = $true)]
+        [string]$domain,
+        [Parameter(Mandatory = $true)]
+        [string]$adminUsername,
+        [Parameter(Mandatory = $true)]
+        [string]$adminPassword
+    )
+    try {
+            # Create a temporary file in the users TEMP directory
+            $file = $env:TEMP + "\JoinDomain.ps1"
+
+            $commands = "`$domainUsername=""$domain\$adminUsername""" + "`r`n"
+            $commands = $commands + "`$domainPassword=""$adminPassword""" + "`r`n"
+            $commands = $commands + "`$SecurePassword = ConvertTo-SecureString `$domainPassword -AsPlainText -Force" + "`r`n"
+            $commands = $commands + "`$credential = New-Object System.Management.Automation.PSCredential (`$domainUsername, `$SecurePassword)" + "`r`n"
+            $commands = $commands + "Add-Computer -DomainName ""$domain.com"" -Credential `$credential -Restart -Force -PassThru -ErrorAction Stop"
+            
+            $commands | Out-File -FilePath $file -force
+
+            $result = Invoke-AzVMRunCommand -ResourceGroupName $resourceGroup -VMName $vmName -CommandId "RunPowerShellScript" -ScriptPath $file
+
+            if ($result.Status -eq "Succeeded") {
+                $message = "$vmName has been joined to domain."
+                NewMessage -message $message -type "success"
+            }
+            else {
+                $message = "$vmName couldn't be joined to domain."
+                NewMessage -message $message -type "error"
+            }
+
+            Remove-Item $file
+    }
+    catch {
+        Remove-Item $file
+        Write-Warning "Error occured = " $Error[0]
+    }
+}
+
+# Add Firewall Rule
+function AddFirewallRule 
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$vmName,
+        [Parameter(Mandatory = $true)]
+        [string]$resourceGroup
+    )
+    try {
+            # Create a temporary file in the users TEMP directory
+            $file = $env:TEMP + "\AddFirewallRule.ps1"
+
+            $commands = "New-NetFirewallRule -DisplayName 'SQLAG' -Profile @('Domain') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('1433', '5022')"
+            $commands | Out-File -FilePath $file -force
+
+            $result = Invoke-AzVMRunCommand -ResourceGroupName $resourceGroup -VMName $vmName -CommandId "RunPowerShellScript" -ScriptPath $file
+
+            if ($result.Status -eq "Succeeded") {
+                $message = "Firewall rule has been added on $vmName."
+                NewMessage -message $message -type "success"
+            }
+            else {
+                $message = "Firewall rule couldn't be added on $vmName."
+                NewMessage -message $message -type "error"
+            }
+
+            Remove-Item $file
+    }
+    catch {
+        Remove-Item $file
+        Write-Warning "Error occured = " $Error[0]
+    }
+}
+
 # Main Code
 
 # Connect to Azure Subscription
 ConnectToAzure -subscriptionId $subscriptionId
 
 # Enable Firewall Rule
-#EnableFirewallRule -resourceGroup $resourceGroup -vmName "DCVM01" -ErrorAction SilentlyContinue
-#EnableFirewallRule -resourceGroup $resourceGroup -vmName "AlwaysOnN1" -ErrorAction SilentlyContinue
+EnableFirewallRule -resourceGroup $resourceGroup -vmName "DCVM01" -ErrorAction SilentlyContinue
+EnableFirewallRule -resourceGroup $resourceGroup -vmName "AlwaysOnN1" -ErrorAction SilentlyContinue
 
 # Install Active Directory Domain Services
-#InstallADDS -resourceGroup $resourceGroup -vmName "DCVM01" -adminPassword "Microsoft123" -ErrorAction SilentlyContinue
+InstallADDS -resourceGroup $resourceGroup -vmName "DCVM01" -adminPassword "Microsoft123" -ErrorAction SilentlyContinue
 
 # Create a new Active Directory Organization Unit and make it default for computer objects
-#NewADOU -resourceGroup $resourceGroup -vmName "DCVM01" -ErrorAction SilentlyContinue
+NewADOU -resourceGroup $resourceGroup -vmName "DCVM01" -ErrorAction SilentlyContinue
 
-# Restart DCVM01 after AD Installation. It is not rebooted in InstallADDS because a new OU is created after install.
-#Restart-AzVM -ResourceGroupName $resourceGroup -Name "DCVM01"
+# Join Azure VM to domain
+JoinDomain -resourceGroup $resourceGroup -vmName "AlwaysOnN1" -domain "contoso" -adminUsername "azadmin" -adminPassword "Microsoft123" -ErrorAction SilentlyContinue
 
+# Add Firewall Rule
+AddFirewallRule -resourceGroup $resourceGroup -vmName "AlwaysOnN1" -ErrorAction SilentlyContinue
 
 
